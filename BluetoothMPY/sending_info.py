d24300d6a259700fcb7efd07dff381a3801fd411
kaushikp0628 <112449814+kaushikp0628@users.noreply.github.com>
2025-02-24 12:42:26 -0500

    Update sending_info.py

--- BluetoothMPY/sending_info.py
+++ BluetoothMPY/sending_info.py
@@ -12,20 +12,56 @@ sp = BLESimplePeripheral(ble)
 # Configure LED as led
 led = Pin("LED", Pin.OUT)
 
+#Declare input pins
+pin1 = Pin(0, Pin.IN, Pin.PULL_DOWN)
+pin2 = Pin(1, Pin.IN, Pin.PULL_DOWN)
+pin3 = Pin(2, Pin.IN, Pin.PULL_DOWN)
+pin4 = Pin(3, Pin.IN, Pin.PULL_DOWN)
+pin5 = Pin(4, Pin.IN, Pin.PULL_DOWN)
+
+#Declare pull down for inactive state
+
+
 while True:
     if sp.is_connected():
             # Create a message string
-            msg="This is foot remote here"
+            msg="Kick-Button Connected"
             # Send the message via BLE
             sp.send(msg)
             time.sleep(1)
             led.value(1)
             time.sleep(1)
             led.value(0)
-            msg = "Hello world"
+            msg = "Initializing transfer connection"
             # Send the message via BLE
             sp.send(msg)
             time.sleep(1)
             led.value(1)
             time.sleep(1)
             led.value(0)
+            
+            if pin1.value() == 1:
+                msg = "UP"
+                sp.send(msg)
+                time.sleep(1)
+            elif pin2.value() == 1:
+                msg = "DOWN"
+                sp.send(msg)
+                time.sleep(1)
+            elif pin3.value() == 1:
+                msg = "LEFT"
+                sp.send(msg)
+                time.sleep(1)
+            elif pin4.value() == 1:
+                msg = "RIGHT"
+                sp.send(msg)
+                time.sleep(1)
+            elif pin5.value() == 1:
+                msg = "CENTER"
+                sp.send(msg)
+                time.sleep(1)
+            else:
+                msg = "No Button Pressed"
+                sp.send(msg)
+                time.sleep(1)
+            
