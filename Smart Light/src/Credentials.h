8a0a2f1cf9fcc640c060fa49d9b48f2ed6cff528
carusoc2025 <112443077+carusoc2025@users.noreply.github.com>
2025-04-09 12:04:31 -0400

    Add files via upload

---
+++ Smart Light/src/Credentials.h
@@ -0,0 +1,23 @@
+// https://github.com/daniloc/PicoW_HomeAssistant_Starter/tree/main/src
+
+
+#ifndef SRC_CREDENTIALS
+#define SRC_CREDENTIALS
+
+
+#define WIFI_SSID "test123"
+#define WIFI_PASSWORD "calumrocks"
+
+
+#define MQTT_LOGIN "pritest"
+#define MQTT_PASSWORD "NoBusPlease"
+
+
+#define MQTT_BROKER IPAddress(10,20,0,1)
+
+
+// Change the last number/argument for your own device, following the guidelines in the drive
+#define CLIENT_IP IPAddress(10,20,0,87)
+
+
+#endif // SRC_CREDENTIALS
---
+++ Smart Light/src/HAIntegration.cpp
@@ -0,0 +1,59 @@
+// Adapted from:
+// https://github.com/daniloc/PicoW_HomeAssistant_Starter/tree/main/src
+
+#include "HAIntegration.h"
+#include "Credentials.h"
+
+#include <ArduinoHA.h>
+#include <WiFi.h>
+
+//Adapted via:
+//  https://github.com/dawidchyrzynski/arduino-home-assistant/blob/main/examples/nano33iot/nano33iot.ino
+
+#define LIGHT_PIN   13
+
+WiFiClient client;
+HADevice device;
+HAMqtt mqtt(client, device);
+HASwitch light("lightREAL");
+
+void HAIntegration::configure() {
+
+    //Prepare Light:
+
+    pinMode(LIGHT_PIN, OUTPUT); //Prepare pin
+    digitalWrite(LIGHT_PIN, LOW); //Set default mode
+    
+    //Set device ID as MAC address
+
+    byte mac[WL_MAC_ADDR_LENGTH];
+    WiFi.macAddress(mac);
+    device.setUniqueId(mac, sizeof(mac));
+
+    //Device metadata:
+
+    device.setName("Pico W HA Starter");
+    device.setSoftwareVersion("0.1");
+
+    // handle switch state
+    light.onCommand(switchHandler);
+    light.setName("Smart Light"); // optional
+
+    Serial.print("Connecting to MQTT\n");
+    
+    if (mqtt.begin(MQTT_BROKER, MQTT_LOGIN, MQTT_PASSWORD) == true) {
+        Serial.print("Connected to MQTT broker");
+    } else {
+        Serial.print("Could not connect to MQTT broker");
+    }
+}
+
+void HAIntegration::switchHandler(bool state, HASwitch* sender) {
+    digitalWrite(LIGHT_PIN, (state ? HIGH : LOW)); //Set light state, default off if null
+    sender->setState(state);  // report state back to Home Assistant
+}
+
+
+void HAIntegration::loop() {
+    mqtt.loop(); 
+}
